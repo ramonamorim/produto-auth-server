c6f5ac2048cfd5223856e77c1e5aea5ce38ab54b
Ramon Amorim <ramon.neogrid@neogrid.com>
2018-04-25 10:46:41 -0300

    initial commit

---
+++ src/main/java/br/com/empresa/produto/produtoauthserver/ProdutoAuthServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.empresa.produto.produtoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoAuthServerApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/empresa/produto/produtoauthserver/ProdutoAuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.empresa.produto.produtoauthserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdutoAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
