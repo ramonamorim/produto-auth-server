19e04ea64eda5dd6518789a659c26e53b0f1242a
unknown <amorimramon@outlook.com>
2018-05-08 00:06:59 -0300

    update

--- src/main/java/br/com/empresa/produto/produtoauthserver/ProdutoAuthServerApplication.java
+++ src/main/java/br/com/empresa/produto/produtoauthserver/ProdutoAuthServerApplication.java
@@ -2,10 +2,12 @@ package br.com.empresa.produto.produtoauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
+@EnableEurekaClient
 public class ProdutoAuthServerApplication {
  
 	public static void main(String[] args) {
